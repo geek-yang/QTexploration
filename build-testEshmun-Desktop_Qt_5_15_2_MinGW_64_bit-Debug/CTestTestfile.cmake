4f20c4ce851a025db417d1e12a64cb55fe821602
Yang <y.liu@esciencecenter.nl>
2021-03-14 16:31:57 +0100

    ignore build folder

--- build-testEshmun-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/NLeSC/CAT/FEDMix/QTexploration/testEshmun
-# Build directory: D:/NLeSC/CAT/FEDMix/QTexploration/build-testEshmun-Desktop_Qt_5_15_2_MinGW_64_bit-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(testEshmun "D:/NLeSC/CAT/FEDMix/QTexploration/build-testEshmun-Desktop_Qt_5_15_2_MinGW_64_bit-Debug/testEshmun.exe")
-set_tests_properties(testEshmun PROPERTIES  _BACKTRACE_TRIPLES "D:/NLeSC/CAT/FEDMix/QTexploration/testEshmun/CMakeLists.txt;26;add_test;D:/NLeSC/CAT/FEDMix/QTexploration/testEshmun/CMakeLists.txt;0;")
